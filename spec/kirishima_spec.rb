6c4005763a534d82f3244358cd10093a94c38b28
Junichi Ito <jit@sonicgarden.jp>
2014-08-30 23:40:43 +0900

    Refactoring.

--- spec/kirishima_spec.rb
+++ spec/kirishima_spec.rb
@@ -4,22 +4,17 @@ describe Kirishima do
   describe '#main' do
     context 'sample 1' do
       let(:programmer_count) { 60 }
-      let(:offers) { [Offer.new(40, 4300), Offer.new(30, 2300), Offer.new(20, 2400)] }
+      let(:offers) { [[40, 4300], [30, 2300], [20, 2400]].map{|values| Offer.new(*values)} }
       specify do
         expect(Kirishima.new.main(programmer_count, offers)).to eq 6600
       end
     end
-    # context 'sample 2' do
-    #   # 35 3640
-    #   # 33 2706
-    #   # 98 9810
-    #   # 57 5472
-    #   # 95 7790
-    #   let(:programmer_count) { 250 }
-    #   let(:offers) { [[35, 3640], [33, 2706], [98, 9810], [57, 5472], [95, 7790]] }
-    #   specify do
-    #     expect(Kirishima.new.main(programmer_count, offers)).to eq 23072
-    #   end
-    # end
+    context 'sample 2' do
+      let(:programmer_count) { 250 }
+      let(:offers) { [[35, 3640], [33, 2706], [98, 9810], [57, 5472], [95, 7790]].map{|values| Offer.new(*values)} }
+      specify do
+        expect(Kirishima.new.main(programmer_count, offers)).to eq 23072
+      end
+    end
   end
 end
