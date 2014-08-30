8d5c226b48354e322e954477d232e577ea3b78eb
Junichi Ito <jit@sonicgarden.jp>
2014-08-30 23:46:20 +0900

    Refactoring.

--- lib/kirishima.rb
+++ lib/kirishima.rb
@@ -1,7 +1,7 @@
 class Kirishima
   def main(programmer_count, offers)
     (1..offers.size).flat_map {|n|
-      offers.combination(n).map{|offers| OfferGroup.new(offers)}
+      offers.combination(n).map{|offers| OfferGroup.new(offers) }
     }.select {|offer_group|
       offer_group.programmer_count >= programmer_count
     }.min_by(&:total_cost).total_cost
@@ -12,17 +12,13 @@ class Offer < Struct.new(:programmer_count, :total_cost)
 end
 
 class OfferGroup
+  attr_reader :programmer_count, :total_cost
   def initialize(offers)
-    @offers = offers
-  end
-  def programmer_count
-    @programmer_count ||= sum_of(:programmer_count)
-  end
-  def total_cost
-    @total_cost ||= sum_of(:total_cost)
+    @programmer_count ||= sum_of(offers, :programmer_count)
+    @total_cost ||= sum_of(offers, :total_cost)
   end
   private
-  def sum_of(attr)
-    @offers.inject(0){|sum, offer| sum + offer.send(attr) }
+  def sum_of(offers, attr)
+    offers.inject(0){|sum, offer| sum + offer.send(attr) }
   end
 end
