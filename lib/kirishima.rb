81cb356380f1558d71bc417e86765506d3959d29
Junichi Ito <jit@sonicgarden.jp>
2014-08-30 23:07:46 +0900

    Initial answer.

--- lib/kirishima.rb
+++ lib/kirishima.rb
@@ -1,5 +1,11 @@
 class Kirishima
   def main(programmer_count, offers)
-    1
+    all_combinations = (1..offers.size).flat_map do |n|
+      offers.combination(n).to_a
+    end
+    counts_and_moneys = all_combinations.map do |offer_array|
+      [offer_array.map(&:first).inject{|sum,x| sum + x }, offer_array.map(&:last).inject{|sum,x| sum + x }]
+    end
+    counts_and_moneys.select{|counts, _| counts >= programmer_count }.min_by(&:last).last
   end
 end
